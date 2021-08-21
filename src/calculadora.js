b74065d09b8f6ceda5c77f6a8dd686c297bccbf5
hailton david lemos <hailton.david@gmail.com>
2021-08-20 22:08:29 -0300

    Mensagem

---
+++ src/calculadora.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './calculadora.css';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+function Calculadora() {
+  return (
+    <Jumbotron style={{
+        background: 'transparent !important',
+        backgroundColor: '#007bff',
+        padding: '5px',
+        margin: '5px',
+        width: '400px'
+      }}>
+      <Container>
+      <Row>
+      <Col xs = "3">
+      <Button variant="danger">
+      C
+      </Button>
+      </Col>
+      <Col xs = "9">
+      <Form.Control type="text"
+        name="txtNumeros"
+        className="text-right"
+        readOnly="readonly"
+        data-testid="txtNumeros" />
+      </Col>
+      </Row>
+      </Container>
+      </Jumbotron>
+  );
+}
+
+export default Calculadora;
--- src/calculadora.test.js
+++ src/calculadora.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Calculadora from './calculadora';
 
 test('renders learn react link', () => {
   render(<App />);
